2826f73604f4ac227d67655d747c2d6c45395be8
NktSH <49846495+NktSH@users.noreply.github.com>
2023-02-23 15:52:15 +0300

    Update variables.sh

--- Monitoring/variables.sh
+++ Monitoring/variables.sh
@@ -2,4 +2,4 @@
 COS_BIN_NAME=          # example: /root/go/bin/babylond or /home/user/go/bin/babylond
 COS_PORT_RPC=          # default: 26657
 COS_VALOPER=           # example: bbnvaloper1234545636767376535673
-LOGENTRY=bbn
+LOGENTRY=babylon
